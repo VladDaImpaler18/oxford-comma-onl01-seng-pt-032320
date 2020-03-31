475422d2f226b67b3ad5235e00b35b265aa020fe
Vladimir J <vladdaimpaler@protonmail.com>
2020-03-31 02:17:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,10 @@ def oxford_comma(array)
 statement=""
 array.each{ |name|
 #if 2nd to last, add ", and"
+
 statement+= "#{name}, " unless (name==array.last || name==array[-2]) && array.size>2
 statement+= "#{name}, and " if array[-2]==name && array.size > 2
-statement+= "#{name}" if array.last==name
+statement+= "#{name}" if array.first==name
 }
 return statement
 end
