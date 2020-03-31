bd375f60da1ca38a75bf62719d6b53afc02bbaab
Vladimir J <vladdaimpaler@protonmail.com>
2020-03-31 02:19:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,6 @@
 def oxford_comma(array)
 statement=""
 array.each{ |name|
-#if 2nd to last, add ", and"
 
 statement+= ", #{name}" unless (name==array.first || name==array.last)
 statement+= "#{name}, and " if array[-2]==name && array.size > 2
